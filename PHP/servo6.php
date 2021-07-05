b3d9b3bc41d6f97c4dc1bc1303f9eb788f1c1c42
anis246 <85651361+anis246@users.noreply.github.com>
2021-07-05 11:15:56 +0100

    Add files via upload

---
+++ PHP/servo6.php
@@ -0,0 +1,23 @@
+<?php 
+
+$host = "localhost";
+$db_name = "robot_arm";
+$username = "root";
+$password = "";
+ 
+$con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+ 
+$query1 = "SELECT servo, value FROM servo WHERE servo = ? LIMIT 0,1";
+$stmt = $con->prepare( $query1 );
+ 
+$servo=6;
+ 
+$stmt->bindParam(1, $servo);
+ 
+$stmt->execute();
+ 
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+echo  $row['value'];
+
+?>
